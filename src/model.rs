0e59700ceb7bfcb0b10b494f076ac63b12f57e0a
Raphael.Talleux <raphael.talleux@gmail.com>
2026-05-01 11:41:58 +0200

    feat: enable decrypting Secret source via getter method

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,15 @@
+use crate::encrypt;
+
 #[derive(Debug)]
 pub struct Secret {
     pub source: String,
-    pub password: String,
+    pub _password: String,
     pub nonce: String,
     pub salt: String,
-    pub is_encrypted: bool
+}
+
+impl Secret {
+    pub fn as_source_plaintext(&self, key: &str) -> String {
+        encrypt::decrypt_data(key, &self.source, &self.nonce, &self.salt).unwrap()
+    }
 }
